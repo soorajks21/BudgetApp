ecb8a1905329eb7d8cbac3f93acdd77375f1690e
soorajks21 <soorajks@live.in>
2019-11-13 11:28:16 -0500

    replaces strings with objects in document.querySelector

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-var budgetController = (function(){
+//budget Controller
+var budgetController = (function() {})();
 
-    var x = 23;
-    var add = function(a){
-        return x + a;
+//UIController
+var UIController = (function() {
+  var Domstings = {
+    inputType: ".add__type",
+    inputDescripton: ".add__description",
+    inputValue: ".add__value",
+    inputBtn:'.add__btn'
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(inputType).value,
+        description: document.querySelector(inputDescripton).value,
+        value: document.querySelector(inputValue).value
+      };
+    },
+    getDomstrings: function(){
+
+        return Domstings;
     }
 
-    return {
-        publicTest: function(b){
-           return add(b);
-        }
-    }
-
-
+  };
 })();
 
-var UIController = (function(){
+//Global app controller
+var controller = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.Domstings;
+  var cntrlAddItem = function() {
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+  document.querySelector(DOM.inputBtn).addEventListener("click", cntrlAddItem);
 
-})();
-
-
-var controller = (function(budgetCtrl, UICtrl){
-
-var z = budgetCtrl.publicTest(5);
-
-return {
-    anotherPublicTest: function(){
-        console.log(z);
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      cntrlAddItem();
     }
-}
-
+  });
 })(budgetController, UIController);
